b9aeb28cd52fb24f66f6171d7f2745f67f1ecb4f
Therisse <laetitia.zaleski@gmail.com>
2017-04-06 13:15:42 +0200

    parc3D

--- src/main/scala/simpark/Parc3D.scala
+++ src/main/scala/simpark/Parc3D.scala
@@ -22,27 +22,37 @@ import viabilitree.model._
 case class Parc3D(
                    integrationStep: Double = 0.01,
                    timeStep: Double = 0.1,
-                   g: Double = 20.0,
-                   s: Double = 1.0,
+                   //zeta: Double = 0.01,
+                   l: Double = 0.01,
+                   g: Double = 1.0,
                    M: Double = 5000.0,
-                   h: Double = 0.001,
-                   b: Double = 1.0,
-                   a: Double = 8.0,
-                   c: Double = 0.5,
-                   r: Double = 1.0,
-                   K: Double = 100.0,
-                   eps: Double = 10
-                   // valeurs de controle: d control(0) et epsilon control(1)
+                   c: Double = 0.01,
+                   p: Double = 0.3,
+                   a: Double = 100.0,
+                   e: Double = 0.001,
+                   eta: Double = 0.0005,
+                   //eps: Double = 10.0,
+                   phi: Double = 1.0,
+                   phi2: Double = 1.0,
+                   d: Double = 1.0,
+                   del: Double = 0.1,
+                   h: Double = 12000.0,
+                   mp: Double = 20.0,
+                   mt: Double = 0.5
+
+                   // valeurs de controle: eps control(1) et zeta control(0)
                  ) {
 
   def dynamic(state: Vector[Double], control: Vector[Double]) = {
     // A: state(0), T: state(1), E: state(2)
-    def ADot(state: Vector[Double], t: Double) = state(0) * g * (1 - state(0)/(1+ M * state(2))) - h * state(1) * state(0)
+    def CDot(state: Vector[Double], t: Double)= -del * state(0) + p*state(1)*mp + mt*state(2)-h
 
-    def TDot(state: Vector[Double], t: Double) = state(1) * ( a * state(0) + b * state(2) - c * state(1) - control(0))
+    def ADot(state: Vector[Double], t: Double) = state(1) * g * (1 - state(1)/(1+ M /(1+eta*state(2)/(control(1)+1)))) -  control(0) * l * state(1) * state(2) - p*state(1)
 
-    def EDot(state: Vector[Double], t: Double)= state(2) * r * (1 - state(2)/K ) - s * state(1) *state(2) + eps * state(1) * (K - state(2))
-    val dynamic = Dynamic(ADot, TDot, EDot)
+    def TDot(state: Vector[Double], t: Double) = state(2) * ( - c * state(2)/(state(2)+phi)+e * state(0)/(state(0)+phi2)-d) + a * control(0)* state(1)
+
+
+    val dynamic = Dynamic(ADot, TDot, CDot)
     dynamic.integrate(state.toArray, integrationStep, timeStep)
   }
 
--- src/main/scala/simpark/package.scala
+++ src/main/scala/simpark/package.scala
@@ -11,17 +11,22 @@ object Parc3DViabilityKernel extends App {
 
   val vk = KernelComputation(
     dynamic = parc.dynamic,
-    depth = 12,
-    zone = Vector((3000.0, 5000.0), (5000.0, 20000.0), (5000.0, 10000.0)),
-    controls = Vector((0.0 to 15000.0 by 1000.0))
-    // controls = Vector((5000.0 to 15000.0 by 1000.0),(10.0 to 200.0 by 10.0))
+    depth = 21,
+    zone = Vector((0.0, 25000.0), (2000.0, 5000.0), (1500.0, 2500.0)),
+    controls = (x: Vector[Double]) =>
+      for {
+        zeta <- (0.01 to 0.02 by 0.01 )
+        eps <- (0.0 to 100.0 by 10.0)
+      } yield Control(zeta, eps)
+
   )
 
   val (ak, steps) = approximate(vk, rng)
 
   println(steps)
 
-  saveVTK2D(ak, "/tmp/resparc3D.vtk")
+  saveVTK3D(ak, s"/tmp/resparcCATDepth${vk.depth}2controls.vtk")
+  saveHyperRectangles(vk)(ak,s"/tmp/resparcCATWithControlD${vk.depth}.txt")
 
   //println(volume(res))
 
